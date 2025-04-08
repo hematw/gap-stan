9b6e8d0107f0894d4afa8b7cd1938c2679ee2b41
Hemat-ATOM <hematullahw@gmail.com>
2025-04-08 23:39:28 +0430

    initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-full h-screen flex items-center justify-center'>
+      <h1 className='text-red-400 text-5xl'>Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
