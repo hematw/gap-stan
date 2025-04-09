d82a2566bbed54efa3d5951c8f66ceb43da33462
Hemat-ATOM <hematullahw@gmail.com>
2025-04-10 00:32:21 +0430

    basic socket setup & cors  in backend

--- server/server.js
+++ server/server.js
@@ -1,11 +1,47 @@
-const express = require('express');
+import dotenv from "dotenv";
+import express from "express";
+import http from "http";
+import { Server as IOServer } from "socket.io";
+import cors from "cors";
+
+dotenv.config();
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",");
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the chat app API!");
 });
 
-const port = 3000;
+
+const server = http.createServer(app);
+const io = new IOServer(server, {
+  cors: { origin: allowedOrigins || ["http://localhost:5173"] },
+  credentials: true,
+});
+
+io.on("connection", (socket) => {
+  console.log("New client connected:", socket.id);
+
+  socket.on("message", (msg) => {
+    console.log("Message received:", msg);
+    io.emit("message", msg); // Broadcast to all
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`);
+  console.log(`app listening on port ${port}!✅`);
 });
